f7f4c8d5279ae221929359b60e900e52bf819296
Роман Данилевич <dwordchanel@gmail.com>
2020-05-19 02:55:50 +0300

    Add project files.

---
+++ OOP_lab_3_14_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace OOP_lab_3_14_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader file = new StreamReader("input.txt");
+
+            StreamWriter newFile = File.CreateText("output.txt");
+
+            newFile.Write(file.ReadToEnd().Length);
+
+            newFile.Close();
+        }
+    }
+}
